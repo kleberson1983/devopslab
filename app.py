0b2fbba3f0dfcf21829c0baebe67417ed72844c5
kleberson1983 <kleberson.brandao@hotmail.com>
2021-10-29 13:01:20 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Hello World"
 
 @app.route('/bug')                                                                                                                                
 def bad():                                                                                                                                        
