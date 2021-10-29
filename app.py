4147c9d6e2aa493cd8771b57aa1ea8727397d643
kleberson1983 <kleberson.brandao@hotmail.com>
2021-10-29 12:47:19 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-csrf = CSRFProtect
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
-@app.route("/bug")                                                                                                                               def bad():                                                                                                                                           try:                                                                                                                                        
-        raise TypeError()                                                                                                                            except TypeError as e:                                                                                                                               print(e)                        
-    except TypeError as e:
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
 
-@app.route("/soma")
-def soma_valores():
-    x=10+10
-    return "Sua soma de 10+10="+str(x)
 
 if __name__ == '__main__':
     app.run()
