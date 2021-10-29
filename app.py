1a5909e62219fd05c822e092bdd17f8edab67143
kleberson1983 <kleberson.brandao@hotmail.com>
2021-10-29 12:40:30 -0300

    Bug Adicionado

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ csrf = CSRFProtect
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
-@app.route('/bug')                                                                                                                               def bad():                                                                                                                                           try:                                                                                                                                        
+@app.route("/bug")                                                                                                                               def bad():                                                                                                                                           try:                                                                                                                                        
         raise TypeError()                                                                                                                            except TypeError as e:                                                                                                                               print(e)                        
     except TypeError as e:
         print("Duplicado, ou seja, nunca vai entrar aqui.")
